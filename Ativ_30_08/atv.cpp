fc753ac3a144eb257f8f4c53b4b43e93c3f78d62
lucasito028 <lucastoshihiroito28@gmail.com>
2023-08-30 14:28:52 -0300

    alterando

--- Ativ_30_08/atv.cpp
+++ Ativ_30_08/atv.cpp
@@ -7,19 +7,57 @@ struct No
     struct No *prox;
 };
 
+
+
 typedef struct No lista;
 
+
+
+void inicializa(lista *cartas);
+
+int checa_lista(lista *cartas);
+
+
+
 int main(void){
-    
-    lista * cartas = (lista *) malloc(sizeof(lista));
+
+    lista *cartas = (lista *) malloc(sizeof(lista));
+
 
     if(!cartas){
-        printf("Impossivel alocar");
+        printf("Nao tem como alocar");
         exit(1);
     }
     else 
-        printf ("Memoria alocada\n");
+        printf ("Deu certo\n");
+
+    inicializa(cartas);
+
+
+    int i = checa_lista(cartas);
+
+    if(i == 1)
+        printf("Lista vazia\n");
+
+    else if ( i == 0){
+        printf("Lista nao vazia\n");
+    }
+
+
 
     free(cartas);
     return 0;
+}
+
+void inicializa(lista *cartas){
+    cartas->prox = NULL;
+    printf("Lista cartas inicalizada");
+}
+
+
+int checa_lista(lista *cartas){
+    if(cartas->prox == NULL)
+        return 1;
+    else 
+        return 0;
 }
